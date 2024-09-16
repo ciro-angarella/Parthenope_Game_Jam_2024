6958b2a5ab1ad9b0ff448b2ff1f8998447a3bbac
ciro-angarella <ciroangarella000@gmail.com>
2024-09-16 22:33:17 +0200

    menu (base)

---
+++ menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+var ButtonContainer : VBoxContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	ButtonContainer  = $MarginContainer/VBoxContainer
+	var screen_size = get_viewport().size
+	# Posiziona il VBoxContainer al centro dello schermo
+	set_global_position(screen_size/2, true) # keep_offsets=true per aggiornare gli anchor
+	
+	#button size
+	for button in ButtonContainer.get_children():
+		button.min_size = Vector2(300,50)
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_play_pressed() -> void:
+	var scene_to_load = "res://scenes/arena.tscn"  
+	var result = get_tree().change_scene_to_file(scene_to_load)
+	
+	if result != OK:
+		print("Errore nel caricamento della scena: ", result)
+
+
+
+func _on_guide_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_credits_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
