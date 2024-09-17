36b41bcff64d1c4ded335d5405b2ea1d3b0c68ce
ciro-angarella <ciroangarella000@gmail.com>
2024-09-17 10:48:05 +0200

    arena fatta

--- menu.gd
+++ menu.gd
@@ -1,10 +1,9 @@
 extends Control
 
-var ButtonContainer : VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	ButtonContainer  = $MarginContainer/VBoxContainer
+	
 	var screen_size = get_viewport().size
 	# Posiziona il VBoxContainer al centro dello schermo
 	set_global_position(screen_size/2, true) # keep_offsets=true per aggiornare gli anchor
--- scripts/credits.gd
+++ scripts/credits.gd
@@ -5,7 +5,7 @@ var Content : VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Content  = $MarginContainer/VBoxContainer
+
 	var screen_size = get_viewport().size
 	# Posiziona il VBoxContainer al centro dello schermo
 	set_global_position(screen_size/2, true) # keep_offsets=true per aggiornare gli anchor
