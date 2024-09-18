1da94c748e1ae63b1ebe5723e5a50a010eea0273
ciro-angarella <ciroangarella000@gmail.com>
2024-09-18 02:37:29 +0200

    prova

--- control.gd
+++ control.gd
@@ -3,7 +3,7 @@ extends Control
 var menu_scene =  "res://scenes/menu.tscn"
 
 func _ready() -> void:
-	#await get_tree().create_timer(5.0).timeout
+	await get_tree().create_timer(3.0).timeout
 	cambia_scena()
 	
 func cambia_scena():
